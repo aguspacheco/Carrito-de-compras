506947edd0cf4022e68406efeedac1f9289f1209
Agustin Pacheco <a_pacheco@dgceit.net>
2025-05-05 13:40:08 -0300

    haciendo un filtrado

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { products as initialProducts } from "./mocks/products.json";
+import { Products } from "./components/Products";
+import { useState } from "react";
+
+function App() {
+  const [products] = useState(initialProducts);
+  const { filters, setFilters } = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  return <Products products={filterProducts(products)} />;
+}
+export default App;
